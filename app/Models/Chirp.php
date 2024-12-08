444d29e25c1b11d37a4cc8059b041182c703991d
Morgane BINAZON <morganebinazon@gmail.com>
2024-12-08 21:14:33 +0100

    seven commit

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
     protected $fillable = ['user_id', 'message', 'created_at', 'updated_at'];
 
 }
